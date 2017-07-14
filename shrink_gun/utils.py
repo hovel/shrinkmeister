bed9d25f82041d1e8e53adc11c5eab0c8b7b6cbe
lampslave <lampslave@gmail.com>
2017-07-14 10:07:00 +0300

    refactoring

--- shrink_gun/utils.py
+++ shrink_gun/utils.py
@@ -1,119 +1,149 @@
-from django.core.files.base import ContentFile
-import boto3
-from urllib2 import urlopen
-from wand.image import Image
-from hashlib import md5
-from django.core.cache import cache
-from django.core import signing
-from django.conf import settings
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
 import re
+from hashlib import md5
 
+import boto3
+from django.conf import settings
+from django.core import signing
+from django.core.cache import cache
+from django.core.files.base import ContentFile
+from django.utils.six.moves.urllib.parse import urljoin
+from django.utils.six.moves.urllib.request import urlopen
+from wand.image import Image
+
+# TODO Alert "Not configured" during systemcheck if constants are not specified
 THUMBNAIL_SERVER_URL = getattr(settings, 'THUMBNAIL_SERVER_URL')
+THUMBNAIL_BUCKET = getattr(settings, 'THUMBNAIL_BUCKET')
+GEOMETRY_PATTERN = re.compile(r'^(?P<x>\d+)?(?:x(?P<y>\d+))?$')
 
-class ImageLikeObject(Object):
-    '''
+
+class ThumbnailError(Exception):
+    pass
+
+
+class ImageLikeObject(object):
+    """
     Image object imitator for templates
     provides easy replacement for easy_thumbnail
-    '''
-    def _init__(self, url, width, height):
+    """
+
+    def __init__(self, url, width, height):
         super(ImageLikeObject, self).__init__()
         self.url = url
         self.width = width
         self.height = height
 
-def calculate_size(self, height, width, target_geometry):
-    '''
-    Calculate target image size based on proportion
-    target_w/target_h = source_w/source_h and preserve aspect ratio
 
-    If aspect ratio (target_w/target_h) < (source_w/source_h) ignore target_h, and ignore target_w otherwise
-    '''
-    geometry_splitted = geometry.strip().split('x')
-    target_w = int(geometry_splitted[0] or 0)
-    try:
-        ystr = re.sub("\D", "", geometry_splitted[1]) # Remove possible ImageMagick options from end of the string
-        target_h = int(ystr or 0)
-    except IndexError:
-        target_h = 0
+def parse_geometry(geometry, ratio=None):
+    m = GEOMETRY_PATTERN.match(geometry)
+    if not m:
+        raise ThumbnailError('Wrong geometry')
+    x = m.group('x')
+    y = m.group('y')
+    if x is None and y is None:
+        raise ThumbnailError('Wrong geometry')
+    if x is not None:
+        x = int(x)
+    if y is not None:
+        y = int(y)
+    if ratio is not None:
+        ratio = float(ratio)
+        if x is None:
+            x = int(round(y * ratio)) or 1
+        elif y is None:
+            y = int(round(x / ratio)) or 1
+    return x, y
 
-    if not target_h and not target_w:
-        raise Exception('wrong geometry')
-
-    if (target_h and ((target_w/target_h) < (width/heigt))) or not target_h:
-        target_h = int(target_w * height / float(width))
-    else:
-        target_w = int(target_h * width / float(height))
-        
-    return (target_w, target_h)
 
 def image_from_url(url):
-    '''
+    """
     Return Wand Image from target url
-    '''
+    """
     stream = urlopen(url)
     img = Image(file=stream)
     return img
 
+
 def image_from_s3(bucket, key):
-    '''
+    """
     Return Wand Image from S3 bucket/key pair
-    '''
+    """
     client = boto3.client('s3')
     stream = client.get_object(Bucket=bucket, Key=key)
-    img = Image(file=stream['Body'])
+    img = Image(file=stream['Body'])  # FIXME maybe `blob`?
     return img
 
-def generate_cache_key(url='', bucket='', key='', size='', extra=''):
-    '''
+
+def generate_cache_key(url='', bucket='', key='', geometry_string='', extra='',
+                       **options):
+    """
     Generate hash key for cache for specific image.
     Pass any addional params (such as database primary key)
     to 'extra' for better detection.
-    '''
-    md = md5(url+bucket+key+size+extra)
+    """
+    # TODO options
+    md = md5(url + bucket + key + geometry_string + extra)
     return md.hexdigest()
 
-def generate_protected_request(bucket, key, size):
-    '''
-    Create encrypted string containing address of source image and target image size
-    '''
-    struct = {
-        'bucket': bucket,
-        'key': key,
-        'size': size
-    }
-    return signing.dumps(struct)
 
-def shrink_and_store(img, size, target_bucket, target_key, cache_key):
-    '''
+def generate_thumbnail_url(**url_data):
+    signed_data = signing.dumps(url_data)
+    url = urljoin(urljoin(THUMBNAIL_SERVER_URL, 'hash/'), signed_data)
+    return url
+
+
+def shrink_and_store(img, cache_key, geometry_string, **options):
+    """
     Sample image to target size,
     Store it on S3
 
-    
     For 'size' provide imagemagick options
     http://www.imagemagick.org/script/command-line-processing.php#geometry
-
-    Return image data stored in Cache
-    '''
-    img.transform(resize=size)
-    out_img = ContentFile(img.make_blob('jpeg'))
+    """
+    img.transform(resize=geometry_string)
+    thumb_filename = '{}.{}'.format(cache_key, img.format)
     client = boto3.client('s3')
-    #TODO Extra Args should be passed via arguments?
-    client.upload_fileobj(out_img, Bucket=target_bucket, Key=target_key, ExtraArgs={'StorageClass': 'REDUCED_REDUNDANCY'})
-    url = client.generate_presigned_url(ClientMethod='get_object', Params={ 'Bucket': target_bucket, 'Key': target_key })
-    image_info = {
-        'url': url,
-        'height': img.height,
-        'width': img.width
+    # TODO Extra Args should be passed via arguments?
+    client.upload_fileobj(
+        Fileobj=ContentFile(img.make_blob(), name=thumb_filename),
+        Bucket=THUMBNAIL_BUCKET, Key=thumb_filename,
+        ExtraArgs={'StorageClass': 'REDUCED_REDUNDANCY'})
+    url = client.generate_presigned_url(ClientMethod='get_object',
+                                        Params={'Bucket': THUMBNAIL_BUCKET,
+                                                'Key': thumb_filename})
+    thumb = ImageLikeObject(url=url, width=img.width, height=img.height)
+    cache.set(cache_key, thumb)
+    return thumb
+
+
+def get_thumbnail(file_, geometry_string, **options):
+    if not hasattr(file_, 'key'):
+        raise ThumbnailError('Only S3 objects are supported')
+
+    bucket = file_.key.bucket
+    key = file_.key.key
+
+    cache_key = generate_cache_key(
+        bucket=bucket, key=key, geometry_string=geometry_string, **options)
+    thumb = cache.get(cache_key, None)
+    if thumb:
+        return thumb
+
+    if not hasattr(file_, 'width') or not hasattr(file_, 'height'):
+        raise ThumbnailError('Wrong image instance')
+
+    ratio = float(file_.width) / file_.height
+    thumb_width, thumb_height = parse_geometry(geometry_string, ratio)
+
+    url_data = {
+        'bucket': bucket,
+        'key': key,
+        'geometry_string': geometry_string,
+        'options': options
     }
-    cache.set(cache_key, image_info)
-    return image_info
+    thumb_url = generate_thumbnail_url(**url_data)
 
-def get_thumbnail(img, target_geometry):
-    width, height = calculate_size(img.width, img.height, target_geometry)
-    #TODO Find how to get bucket/key from image object
-    bucket = img.storage.bucket_name
-    key = img.url
-    url = THUMBNAIL_SERVER_URL + generate_protected_request(bucket, key, target_geometry)
-    return ImageLikeObject(url, width, height)
-
-
+    thumb = ImageLikeObject(thumb_url, thumb_width, thumb_height)
+    return thumb
--- shrink_gun/views.py
+++ shrink_gun/views.py
@@ -1,46 +1,56 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.http import HttpResponseRedirect, Http404
-
-from django.views.generic.edit import FormView
-from django.views.generic.base import RedirectView
-from forms import ImageURLForm
-from urllib2 import urlopen
-from wand.image import Image
-import boto3
-from django.core.files.base import ContentFile
-from django.conf import settings
-from utils import image_from_url, image_from_s3, shrink_and_store, generate_cache_key
 from django.core import signing
+from django.core.cache import cache
+from django.http import HttpResponseRedirect, Http404
+from django.views.generic.base import RedirectView
+from django.views.generic.edit import FormView
+
+from forms import ImageURLForm
+from utils import image_from_url, image_from_s3, shrink_and_store, \
+    generate_cache_key
 
 
 class ThumbnailFromURL(FormView):
     form_class = ImageURLForm
     template_name = 'shrink_gun/url_form.html'
-    #TODO Alert "Not configured" during systemcheck if bucket not specified
-    bucket = getattr(settings, 'SHRINK_BUCKET', 'shrinktest')
-    #TODO This should be readed from GET
-    sample_size = '500x500'
+    # TODO This should be readed from GET
+    sample_geometry_string = '500x500'
 
     def form_valid(self, form):
         url = form.cleaned_data['url']
+
+        cache_key = generate_cache_key(
+            url=url, geometry_string=self.sample_geometry_string)
+        thumb = cache.get(cache_key, None)
+        if thumb:
+            return thumb.url
+
         img = image_from_url(url)
-        key = generate_cache_key(url=url, size=self.sample_size)
-        img_info = shrink_and_store(img, self.sample_size, self.bucket, key+'.jpeg', key)
-        return HttpResponseRedirect(img_info['url'])
+        thumb = shrink_and_store(img, cache_key, self.sample_geometry_string)
+        return HttpResponseRedirect(thumb.url)
 
 
 class ThumbnailFromHash(RedirectView):
-    bucket = getattr(settings, 'SHRINK_BUCKET', 'shrinktest')
+
     def get_redirect_url(self, *args, **kwargs):
-        obj = kwargs['hash']
         try:
-            obj = signing.loads(obj)
-        except:
-            raise Http404
-        img = image_from_s3(obj['bucket'], obj['key'])
-        key = generate_cache_key(bucket=obj['bucket'], key=obj['key'], size=obj['size'])
-        img_info = shrink_and_store(img, obj['size'], self.bucket, key+'.jpeg', key)
-        return img_info['url']
-                    
+            data = signing.loads(kwargs['hash'])
+        except Exception as e:
+            raise Http404()
+
+        bucket = data['bucket']
+        key = data['key']
+        geometry_string = data['geometry_string']
+        options = data['options']
+
+        cache_key = generate_cache_key(
+            bucket=bucket, key=key, geometry_string=geometry_string, **options)
+        thumb = cache.get(cache_key, None)
+        if thumb:
+            return thumb.url
+
+        img = image_from_s3(bucket, key)
+        thumb = shrink_and_store(img, cache_key, geometry_string, **options)
+        return thumb.url
